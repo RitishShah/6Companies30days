889d3efd13711c197fc1a05309a74d9eba6b25ee
RitishShah <91522163+RitishShah@users.noreply.github.com>
2022-06-20 08:46:17 +0530

    Update question13.cpp

--- Walmart/question13.cpp
+++ Walmart/question13.cpp
@@ -3,17 +3,19 @@ public:
     // It will sort vector in decreasing order.
     struct comparator{
         bool operator() (string s1, string s2){
-            // if s1 is greater than s2 then it requires swap.
+            // if s1 is greater than s2 then it doesn't requires swap.
             if(s1.size() > s2.size()) return true;
             
-            // if s1 is smaller than s2 then it don't require swap.
+            // if s1 is smaller than s2 then it require swap.
             else if(s1.size() < s2.size()) return false;
             
             // if s1 == s2 then we'll iterate each index of both strings to find greater string.
             else{
                 for(int i=0;i<s1.size();i++){
+                    // No Swap
                     if(s1[i]-'0' > s2[i]-'0') return true;
                     
+                    // Swap
                     else if(s1[i]-'0' < s2[i]-'0') return false;
                 }
                 // if both strings are same then if we do swap or not, doesn't matter.
